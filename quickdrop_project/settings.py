ab3d649c26122c490093a73be3a2cf308c216e1a
Nightshift5 <nightshift5@Trilokn-Infotechs-Mac-mini-2.local>
2026-08-11 03:39:35 +0530

    Switch database to SQLite for Render deployment

--- quickdrop_project/settings.py
+++ quickdrop_project/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'quickdrop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Demo',        # Your database name
-        'USER': 'postgres',          # Your PostgreSQL username
-        'PASSWORD': '1478', # Your PostgreSQL password
-        'HOST': 'localhost',         # Usually localhost
-        'PORT': '5432',              # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
